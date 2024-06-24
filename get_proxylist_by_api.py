d3455dcae4fc869ead0af696abc1f947ed0b2277
liltousin <sergeypletnev2004@gmail.com>
2024-06-25 02:01:43 +0300

    Added headers to the request to avoid bugs

--- get_proxylist_by_api.py
+++ get_proxylist_by_api.py
@@ -11,7 +11,11 @@ BESTPROXIES_APIKEY = os.getenv("BESTPROXIES_APIKEY")
 
 
 def get_new_unused_proxies():
-    result = requests.get(f"http://api.best-proxies.ru/proxylist.txt?key={BESTPROXIES_APIKEY}&uptime=1&limit=0")
+    headers = requests.utils.default_headers()
+
+    headers.update({"User-Agent": "My User Agent 1.0"})
+
+    result = requests.get(f"https://api.best-proxies.ru/proxylist.txt?key={BESTPROXIES_APIKEY}&uptime=1&limit=0", headers=headers)
     print(result.text)
     with open("used_proxies.txt") as file:
         used_proxies = [i.rstrip() for i in file.readlines()]
@@ -23,3 +27,10 @@ def get_new_unused_proxies():
     )
     random.shuffle(data)
     return data
+
+
+if __name__ == "__main__":
+    data = get_new_unused_proxies()
+
+    with open("proxylist.txt", "w") as file:
+        file.write(("\n".join(data) + "\n"))
